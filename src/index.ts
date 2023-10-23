6451d6c7e64bb68c3021309639e0e434b57200d0
0hyeon <djdjdjk2006@naver.com>
2023-10-23 16:23:08 +0900

    feat: adid 암호화

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,30 @@ interface TrackingData {
   sex?: string
   type?: string
 }
+let CryptoJS = require('crypto-js')
+const ciphertext = CryptoJS.AES.encrypt('암호화할 값', 'secret key').toString()
+
 const curry =
-  <A, B, C, D, E>(f: (a: A, b: B, c: C, d: D) => E) =>
+  <A, B, C, D, E, F>(f: (a: A, b: B, c: C, d: D, e: E) => F) =>
   (a: A) =>
   (b: B) =>
   (c: C) =>
   (d: D) =>
-    f(a, b, c, d)
+  (e: E) =>
+    f(a, b, c, d, e)
 
 async function sendTrackingData1(
   name: string,
   event: string,
   sex: string,
   type: string,
+  adid: string,
 ) {
   try {
+    const adid = CryptoJS.AES.encrypt(name, 'secret key').toString()
+    console.log('adid : ', adid)
     const response = await axios.get(
-      `https://web-crawaling.vercel.app/api/post-tracking?name=${name}&event=${event}&sex=${sex}&type=${type}`,
+      `https://web-crawaling.vercel.app/api/post-tracking?name=${name}&event=${event}&sex=${sex}&type=${type}adid=${adid}`,
     )
     return response.data
   } catch (error) {
